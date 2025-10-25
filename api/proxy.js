8807f9146482dee799466c6d8502f1221820b5ed
CRCRCRCRCRC <corrty12345678901234567890@gmail.com>
2025-10-25 14:07:02 +0800

    update

---
+++ api/proxy.js
@@ -0,0 +1,24 @@
+﻿const path = require('path');
+const url = require('url');
+
+const app = require(path.join(__dirname, '..', 'backend', 'server'));
+
+module.exports = (req, res) => {
+  const parsed = url.parse(req.url, true);
+  const slugRaw = parsed.query?.slug || '';
+  const slugValue = Array.isArray(slugRaw) ? slugRaw.join('/') : slugRaw;
+  const cleanedSlug = slugValue ? decodeURIComponent(slugValue).replace(/^\/+/, '') : '';
+
+  const restQuery = { ...parsed.query };
+  delete restQuery.slug;
+  const search = new URLSearchParams(restQuery);
+  const queryString = search.toString();
+
+  const nextPath = `/api${cleanedSlug ? `/${cleanedSlug}` : ''}`;
+  const nextUrl = queryString ? `${nextPath}?${queryString}` : nextPath;
+
+  req.url = nextUrl;
+  req.originalUrl = nextUrl;
+
+  return app(req, res);
+};
